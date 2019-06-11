7d2c18914e693e6d45823111662b642ee4d66098
steinbac <steinbac@mpi-cbg.de>
2019-06-11 22:29:13 +0200

    do a check to see all failing tests

--- code/cpp/std_normal_distribution/test_std_normal.cpp
+++ code/cpp/std_normal_distribution/test_std_normal.cpp
@@ -12,17 +12,17 @@ TEST_CASE( "random numbers diced", "[rng]" ) {
     // standard deviation affects the dispersion of generated values from the mean
     std::normal_distribution<> d{5,2};
 
-    REQUIRE( d(gen) == Approx(3.8995310112) );
-    REQUIRE( d(gen) == Approx(6.0308661394) );
-    REQUIRE( d(gen) == Approx(5.9477217113) );
-    REQUIRE( d(gen) == Approx(7.7369002444) );
-    REQUIRE( d(gen) == Approx(3.1663462535) );
+    CHECK( d(gen) == Approx(3.8995310112) );
+    CHECK( d(gen) == Approx(6.0308661394) );
+    CHECK( d(gen) == Approx(5.9477217113) );
+    CHECK( d(gen) == Approx(7.7369002444) );
+    CHECK( d(gen) == Approx(3.1663462535) );
 
-    REQUIRE( d(gen) == Approx(4.7517056501) );
-    REQUIRE( d(gen) == Approx(0.9780742507) );
-    REQUIRE( d(gen) == Approx(4.0143930558) );
-    REQUIRE( d(gen) == Approx(5.7851594828) );
-    REQUIRE( d(gen) == Approx(3.1416306964) );
+    CHECK( d(gen) == Approx(4.7517056501) );
+    CHECK( d(gen) == Approx(0.9780742507) );
+    CHECK( d(gen) == Approx(4.0143930558) );
+    CHECK( d(gen) == Approx(5.7851594828) );
+    CHECK( d(gen) == Approx(3.1416306964) );
 
 
 }
