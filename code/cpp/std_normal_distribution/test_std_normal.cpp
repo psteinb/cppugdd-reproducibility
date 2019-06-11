dd7cd1db7b921b6ae11fb9631dbe21348b7c7550
Peter Steinbach <steinbach@scionics.de>
2019-06-11 17:03:47 +0200

    renamed externals

--- code/cpp/std_normal_distribution/test_std_normal.cpp
+++ code/cpp/std_normal_distribution/test_std_normal.cpp
@@ -1,8 +1,10 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include <cmath>
+#include <random>
 
-TEST_CASE( "powers are computed", "[powers]" ) {
+TEST_CASE( "random numbers diced", "[rng]" ) {
+
+
     REQUIRE( std::pow(1,2) == 1 );
     REQUIRE( std::pow(2,2) == 4 );
     REQUIRE( std::pow(3,2) == 9 );
