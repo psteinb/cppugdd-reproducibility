4472b22c8c1f39b22e82822a5c986f032e1b33a2
Peter Steinbach <steinbach@scionics.de>
2019-06-11 17:10:13 +0200

    plain test of normal distribution using double precision

--- code/cpp/std_normal_distribution/test_std_normal.cpp
+++ code/cpp/std_normal_distribution/test_std_normal.cpp
@@ -4,8 +4,25 @@
 
 TEST_CASE( "random numbers diced", "[rng]" ) {
 
+    //taken from https://en.cppreference.com/w/cpp/numeric/random/normal_distribution
+    //std::random_device rd{};
+    std::mt19937 gen{42};
 
-    REQUIRE( std::pow(1,2) == 1 );
-    REQUIRE( std::pow(2,2) == 4 );
-    REQUIRE( std::pow(3,2) == 9 );
+    // values near the mean are the most likely
+    // standard deviation affects the dispersion of generated values from the mean
+    std::normal_distribution<> d{5,2};
+
+    REQUIRE( d(gen) == Approx(3.8995310112) );
+    REQUIRE( d(gen) == Approx(6.0308661394) );
+    REQUIRE( d(gen) == Approx(5.9477217113) );
+    REQUIRE( d(gen) == Approx(7.7369002444) );
+    REQUIRE( d(gen) == Approx(3.1663462535) );
+
+    REQUIRE( d(gen) == Approx(4.7517056501) );
+    REQUIRE( d(gen) == Approx(0.9780742507) );
+    REQUIRE( d(gen) == Approx(4.0143930558) );
+    REQUIRE( d(gen) == Approx(5.7851594828) );
+    REQUIRE( d(gen) == Approx(3.1416306964) );
+
+
 }
